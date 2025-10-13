e6514bb0c59f205b4cd3ff2f10478885b2089dd0
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2025-10-13 16:26:20 +0100

    gitignore is updated and CSharp Non-Primitive datatype Class is added

---
+++ _07_ClassDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07_ClassDemo
+{
+    class Person
+    {
+        public string? Name;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {Name}");
+        }
+    }
+
+    static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person class usage
+            Person person = new Person();
+            person.Name = "Nikhilesh";
+            person.Introduce();
+
+
+            // Calculator class usage
+            int number = Calculator.Add(5, 10);
+
+            Console.WriteLine($"The sum is: {number}");
+        }
+    }
+}
