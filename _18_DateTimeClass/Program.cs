ef95a1d9fac5b01f654cff112cd389f8994efd13
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2025-10-16 07:37:37 +0100

    Date Time Class Example is added

---
+++ _18_DateTimeClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _18_DateTimeClass 
+{ 
+    class Program 
+    {
+        static void Main() 
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: {0}" + now.Hour);
+            Console.WriteLine("Minute: {0}" + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yy-MM-dd"));
+        }
+    }
+}
