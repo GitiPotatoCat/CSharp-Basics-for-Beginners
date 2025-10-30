833c7e9072f2bcef7e21e75bb9f228ff64d03e62
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2025-10-30 14:52:07 +0000

    Params Modifier in C# is added

---
+++ _22_ParamsModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _22_ParamsModifier 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Calculator calculator = new Calculator();
+            int resultArrayOne = calculator.Add(1, 2, 3);
+            int resultArrayTwo = calculator.Add(10, 20, 30, 40);
+
+            Console.WriteLine("Sum of 1, 2, 3: " + resultArrayOne);
+            Console.WriteLine("Sum of 10, 20, 30, 40: " + resultArrayTwo);
+        }
+    }
+}
