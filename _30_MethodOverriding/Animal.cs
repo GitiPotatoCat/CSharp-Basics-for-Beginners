4fb59e3346cd39286d831f9d6f8d3ff18fc62ca7
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2025-11-06 05:26:49 +0000

    Method Overriding is added

---
+++ _30_MethodOverriding/Animal.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace _30_MethodOverriding 
+{
+    // Base class
+    public class Animal 
+    {
+        // Virtual method in base class that can be overridden
+        public virtual void MakeSound() 
+        {
+            Console.WriteLine("Some generic animal sound.");
+        }
+    }
+
+
+    // Dervied class
+    public class Dog : Animal 
+    {
+        // Override keyword indicates this method is overrinding
+        public override void MakeSound() 
+        {
+            Console.WriteLine("Woof! Woof!");
+        }
+    }
+
+
+    // Another derived class
+    public class Cat : Animal 
+    {
+        // Different implementation for the same method
+        public override void MakeSound() 
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+}
---
+++ _30_MethodOverriding/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _30_MethodOverriding 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+            Animal theAnimal = new Animal();
+
+            // Call the MakeSound method on each object
+            Console.WriteLine("Dog sound:");
+            myDog.MakeSound();
+
+            Console.WriteLine("\nCat sound:");
+            myCat.MakeSound();
+
+            Console.WriteLine("\nGeneric animal sound:");
+            theAnimal.MakeSound();
+        }
+    }
+}
